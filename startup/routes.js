4c3c7ac83210051a84202d4c9377acdec9ddbe29
aamidou <akimayena26@gmail.com>
2025-04-22 11:43:05 +0100

    fix CSP issue on production

--- startup/routes.js
+++ startup/routes.js
@@ -42,7 +42,26 @@ module.exports = function (app) {
   app.use(cors())
 
   // secure headers
-  app.use(helmet({ crossOriginEmbedderPolicy: false }))
+
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        //CSP issue on production
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: [
+            "'self'",
+            // Add the hash from my browser error: need to be updated
+            "'sha256-ZomnyosL2bmZ79LmErHEhL+1fVaBj9NngvpOK/l4qio='",
+          ],
+          styleSrc: ["'self'", "'unsafe-inline'"], // Optional, needed if styles are inline too
+          objectSrc: ["'none'"],
+          imgSrc: ["'self'", 'data:'], // Allow images if needed
+        },
+      },
+    })
+  )
 
   app.use(cookieParser())
 
